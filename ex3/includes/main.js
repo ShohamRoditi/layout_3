3c590786ec40cd084b9ef838c2a099d90df60a6f
ShohamRoditi <shohamroditi@gmail.com>
2018-05-22 14:28:13 +0300

    ex3

--- ex3/includes/main.js
+++ ex3/includes/main.js
@@ -1,28 +1,41 @@
-(function (){
-	var i;
-	for(i=0;i<4;i++){
-		var newObj = document.createElement("article");
-		var formObj = document.getElementById("sec");
- 		formObj.appendChild(newObj);
-	}
-		
+window.onload = function(){
+ 	var i;
+ 	var plus = document.getElementById("plus");
+ 	for(i=1;i<4;i++){
+ 		createBox();
+ 	}
+ 	plus.onclick=function(){
+		createBox();	
+ 	}
  
- 		// newObj.className ="box";
-		
-	// var opacity_element= newObj.style.opacity; // save the original opacity of the elements
-//  		
-	// newObj.addEventListener("onmouseover", function(){
-    	// newObj.style.opacity = Math.random();
-		 // });
-// 
+
+
+function createBox(){
+	var newObj = document.createElement("article");
+	var formObj = document.getElementById("sec");
+ 	formObj.appendChild(newObj);
+ 	newObj.style.backgroundColor = "#000000";
+ 	newObj.style.opacity = Math.random();
+ 	
+ 	// newObj.addEventListener("onmouseover", function(){
+    // newObj.style.opacity = 1.0;
+    // newObj.style.background="#FFFFFF";
+	// });
+	newObj.onmouseover = function(){
+		opacity_element=newObj.style.opacity;
+		newObj.style.opacity = 1.0;
+    	newObj.style.backgroundColor="#ffffff";
+	}
+	
+	newObj.onmouseout = function(){
+		newObj.style.opacity = opacity_element;
+    	newObj.style.backgroundColor="#000000";
+	}
+	
 	// newObj.addEventListener("onmouseout", function(){
-    	// newObj.style.opacity = opacity_element;
-		 // });
-	
-})();
+    // newObj.style.opacity = opacity_element;
+    // newObj.style.backgroundColor="#000000";
+ 	// });
+}	
 
-//var plus = document.getElementById("button");
-//var.onclick=function(){
-	//makebox;
-	
-
+}
