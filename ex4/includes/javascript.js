7aa95c7f81293c1e45189ee918153a0248b6877e
ShohamRoditi <shohamroditi@gmail.com>
2018-05-28 19:24:23 +0300

    ex4

--- ex4/includes/javascript.js
+++ ex4/includes/javascript.js
@@ -1,27 +1,23 @@
 window.onload=function(){
-	console.log("check");
-	var arr=["yoni","yosi","david","stam"];
-	var address=document.getElementById("address");
 	console.log("check2");
-	var button=document.getElementById;
+	var change=document.getElementById("change");
 	console.log("check3");
-	button.onclick = function(){
-	console.log("check3");
-	// var arr=["yoni","yosi","david","stam"];
-	// var address=document.getElementById("address");
+	
+	change.onclick = function(){
+	console.log("check4");
+	var arr=["yoni","yosi","david","stam"];
+	var address=document.getElementById("address").value;;
 	var i;
 	for(i=0;i<4;i++){
 		if (address==arr[i]){
 			 document.getElementById("myForm").submit();
 		}
 		else{
-			// document.getElementById("err").innerHTML = "you need to inter the propriate address";
+			document.getElementById("error").innerHTML = "<h4>"+ "you need to inter the propriate address"+"<h4>";
 			 document.getElementById("myForm").preventDefault();
-			document.write("you need to inter");
+			// document.write("you need to inter");
 		}	
 	};
-	
-
 	};
 };
 	
